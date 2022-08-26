733e54b1befa5b77d4f68616e2a450d70ceaa94d
heanware <heanware@gmail.com>
2022-08-26 14:00:41 +0500

    js + html refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,69 @@
-$(window).on("load", function () {
+$(function () {
+
     let effect = new Rellax(".rellax-img", {speed: 3});
-})
-$(window).on("scroll", function () {
-    let $videos = $(".js-wider");
-    let $scroll = $(this).scrollTop();
-    let $scrollBarWidth = window.innerWidth - $(window).width();
 
-    $videos.each(function () {
-        let $videos = $(this);
-        let $offset = $videos.offset().top;
-        let width = 1920 - $scrollBarWidth;
-        let $video = $videos.children();
-        if ($scroll > $offset - 300) {
-            $video.css("max-width", width + "px");
-        } else {
-            $video.css("max-width", "1800px");
-        }
+    let $videos = $(".js-wider"),
+        scrollBarWidth = window.innerWidth - $(window).width(),
+        $sliders = $(".js-slider"),
+        scrollPrev = 0,
+        isScrolling = false,
+        verticalHeight = $(window).outerHeight();
+
+    $(window).on("scroll", function (e) {
+
+        let scroll = $(this).scrollTop();
+
+        $videos.each(function () {
+            let $videos = $(this),
+                offset = $videos.offset().top,
+                width = 1920 - scrollBarWidth,
+                $video = $videos.children();
+            if (scroll > offset - 300) {
+                $video.css("max-width", width + "px");
+            } else {
+                $video.css("max-width", "1800px");
+            }
+        });
+        
+        $sliders.each(function () {
+            let $wrapper = $(this),
+                sliderOffset = $wrapper.offset().top,
+                $slider = $wrapper.find(".sticky-slider"),
+                $active = $slider.find(".slide-active"),
+                $slides = $slider.find(".slider").children(),
+                currentSlideIndex = Math.floor(($slider.offset().top - sliderOffset) / verticalHeight);
+
+            if ($active.length <= 0) {
+                $slides.eq(currentSlideIndex).addClass("slide-active");
+            }
+
+            if (scrollPrev === 0) {
+                scrollPrev = scroll;
+            }
+
+            if (scroll > sliderOffset && !isScrolling) {
+
+                let $next = $active.next(),
+                    $prev = $active.prev();
+
+                if (scroll - scrollPrev > 200 && $next.length > 0) {
+                    scrollSlider(sliderOffset + verticalHeight * $next.index(), $active, $next);
+                } else if (scroll - scrollPrev < -200 && $prev.length > 0) {
+                    scrollSlider(sliderOffset + verticalHeight * $prev.index(), $active, $prev);
+                }
+            }
+        })
     });
 
-    let $sliderContainer = $(".js-slider");
-    $sliderContainer.each(function () {
-        let $verticalH = $(window).height();
-        let $container = $(this);
-        let $slides = $container.find(".slider");
-        let $parentOffset = $container.offset().top;
-        let $sliderOffset = $slides.offset().top;
-        let containerScroll = $sliderOffset - $parentOffset;
-        let slidesPassed = Math.floor(containerScroll / $verticalH);
-        let $next = $slides.children().eq(parseInt(slidesPassed));
-        if (!$next.hasClass("slide-active")) {
-            let $active = $slides.find(".slide-active");
-            $active.removeClass("slide-active");
-            $next.addClass("slide-active");
-            $("html").animate({
-                scrollTop: parseInt($parentOffset + $verticalH * slidesPassed)
-            }, 900, "linear")
-        }
-    });
+    function scrollSlider(range, $active, $slide) {
+        isScrolling = true;
+        $active.removeClass("slide-active");
+        $slide.addClass("slide-active");
+        $("html").animate({
+            scrollTop: range
+        }, 1000, "linear", function () {
+            isScrolling = false;
+            scrollPrev = range;
+        });
+    }
 });
